43ff5eac33e045b72807f72c58077a524b3bd02d
VinniMariaI <84975398+VinniMariaI@users.noreply.github.com>
2021-06-10 13:09:09 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,75 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using SENG8040_Assignment1;
+
+namespace RectangleTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        Rectangle rec = new Rectangle();
+
+        [Test]
+       
+        public void GetLengthTestMethod()
+        {
+            Rectangle rec = new Rectangle();
+            int RectangleLength = rec.GetLength();
+            NUnit.Framework.Assert.AreEqual(RectangleLength, 1);
+
+        }
+        [Test]
+        [TestCase]
+        public void GetWidthTestMethod()
+        {
+           Rectangle rec = new Rectangle();
+            int RectangleWidth = rec.GetWidth();
+            NUnit.Framework.Assert.AreEqual(RectangleWidth, 1);
+
+        }
+
+        [Test]
+        [TestCase(10)]
+        [TestCase(20)]
+
+        public void setLengthTestMethod(int length)
+        {
+            rec.setLength(length);
+            NUnit.Framework.Assert.AreEqual(length, rec.GetLength());
+        }
+
+        [Test]
+        [TestCase(20)]
+        [TestCase(15)]
+
+        public void setWidthTestMethod(int width)
+        {
+            rec.setWidth(width);
+            NUnit.Framework.Assert.AreEqual(width, rec.GetWidth());
+        }
+
+        [Test]
+        [TestCase(2,3,10)]
+        [TestCase(3, 3, 12)]
+        public void GetperimeterTestMethod(int length, int width, int expectedPerimeter)
+        {
+            rec.setLength(length);
+            rec.setWidth(width);
+            int Perimeter = rec.GetPerimeter();
+            NUnit.Framework.Assert.AreEqual(Perimeter, expectedPerimeter);
+        }
+
+        [Test]
+        [TestCase(2, 3, 6)]
+        [TestCase(5, 5, 25)]
+        public void GetAreaTestMethod(int length, int width, int expectedArea)
+        {
+            rec.setLength(length);
+            rec.setWidth(width);
+            int Perimeter = rec.GetArea();
+            NUnit.Framework.Assert.AreEqual(Perimeter, expectedArea);
+        }
+
+
+    }
+}
